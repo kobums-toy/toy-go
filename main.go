b2008178e6378c5c05da894c5cc3419493003746
kobums <kobums@naver.com>
2023-08-25 20:39:34 +0900

    feat config.json.docker connectionString

--- main.go
+++ main.go
@@ -27,7 +27,7 @@ func main() {
 	app.Use(cors.New(cors.Config{
 		AllowHeaders: "Origin, Content-Type, Authorization, Accept",
 		AllowCredentials: true,
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "http://141.164.56.77:3000, http://localhost:3000",
 		AllowMethods: strings.Join([]string{
 			fiber.MethodGet,
 			fiber.MethodPost,
